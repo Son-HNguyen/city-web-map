fc86581f87268e0f4bc2bb28926db54672001222
Son H. Nguyen <son.nguyen@tum.de>
2021-08-06 04:08:38 +0200

    Fix resetting grid layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ import {UtilityService} from "../utils.service";
 })
 export class AppComponent implements OnInit {
   title = 'city-web-map';
-  readonly layouts: any;
-  readonly itemPosLayouts: any;
+  layouts: GridLayouts;
+  itemPosLayouts: GridItemPositions;
   options!: GridsterConfig;
   dashboard!: Array<GridsterItem>;
   itemPos: GridItemPos | undefined;
@@ -23,73 +23,75 @@ export class AppComponent implements OnInit {
     private cookieService?: CookieService,
     private GLOBALS?: GlobalService,
     private UTILS?: UtilityService) {
-    this.layouts = {};
-    this.itemPosLayouts = {};
-    this.layouts.layoutLeftGlobe = [
-      {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
-      {cols: 12, rows: 2, y: 1, x: 0}, // Nav
-      {cols: 12, rows: 2, y: 19, x: 0}, // Status
-      {cols: 5, rows: 10, y: 1, x: 12}, // Info
-      {cols: 5, rows: 10, y: 11, x: 12}, // View list
-      {cols: 3, rows: 5, y: 1, x: 17}, // Layer list
-      {cols: 3, rows: 12, y: 6, x: 17}, // Context menu
-      {cols: 3, rows: 3, y: 18, x: 17}, // Menu
-      {cols: 12, rows: 16, y: 3, x: 0} // Cesium app
-    ];
-    this.itemPosLayouts.layoutLeftGlobe = {
-      menuBar: 0,
-      nav: 1,
-      status: 2,
-      info: 3,
-      viewList: 4,
-      layerList: 5,
-      menuContext: 6,
-      menu: 7,
-      globe: 8
+    this.layouts = {
+      layoutLeftGlobe: [
+        {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
+        {cols: 12, rows: 2, y: 1, x: 0}, // Nav
+        {cols: 12, rows: 2, y: 19, x: 0}, // Status
+        {cols: 5, rows: 10, y: 1, x: 12}, // Info
+        {cols: 5, rows: 10, y: 11, x: 12}, // View list
+        {cols: 3, rows: 5, y: 1, x: 17}, // Layer list
+        {cols: 3, rows: 12, y: 6, x: 17}, // Context menu
+        {cols: 3, rows: 3, y: 18, x: 17}, // Menu
+        {cols: 12, rows: 16, y: 3, x: 0} // Cesium app
+      ],
+      layoutCenterGlobe: [
+        {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
+        {cols: 3, rows: 5, y: 1, x: 0}, // Layer list
+        {cols: 3, rows: 12, y: 6, x: 0}, // Context menu
+        {cols: 3, rows: 3, y: 18, x: 0}, // Menu
+        {cols: 12, rows: 2, y: 1, x: 3}, // Nav
+        {cols: 5, rows: 10, y: 1, x: 15}, // Info
+        {cols: 5, rows: 10, y: 11, x: 15}, // View list
+        {cols: 12, rows: 2, y: 19, x: 3}, // Status
+        {cols: 12, rows: 16, y: 3, x: 3} // Cesium app
+      ],
+      layoutRightGlobe: [
+        {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
+        {cols: 3, rows: 5, y: 1, x: 0}, // Layer list
+        {cols: 3, rows: 12, y: 6, x: 0}, // Context menu
+        {cols: 3, rows: 3, y: 18, x: 0}, // Menu
+        {cols: 5, rows: 10, y: 1, x: 3}, // Info
+        {cols: 5, rows: 10, y: 11, x: 3}, // View list
+        {cols: 12, rows: 2, y: 1, x: 8}, // Nav
+        {cols: 12, rows: 2, y: 19, x: 8}, // Status
+        {cols: 12, rows: 16, y: 3, x: 8} // Cesium app
+      ]
     };
-    this.layouts.layoutCenterGlobe = [
-      {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
-      {cols: 3, rows: 5, y: 1, x: 0}, // Layer list
-      {cols: 3, rows: 12, y: 6, x: 0}, // Context menu
-      {cols: 3, rows: 3, y: 18, x: 0}, // Menu
-      {cols: 12, rows: 2, y: 1, x: 3}, // Nav
-      {cols: 5, rows: 10, y: 1, x: 15}, // Info
-      {cols: 5, rows: 10, y: 11, x: 15}, // View list
-      {cols: 12, rows: 2, y: 19, x: 3}, // Status
-      {cols: 12, rows: 16, y: 3, x: 3} // Cesium app
-    ];
-    this.itemPosLayouts.layoutCenterGlobe = {
-      menuBar: 0,
-      layerList: 1,
-      menuContext: 2,
-      menu: 3,
-      nav: 4,
-      info: 5,
-      viewList: 6,
-      status: 7,
-      globe: 8
-    };
-    this.layouts.layoutRightGlobe = [
-      {cols: 20, rows: 1, y: 0, x: 0}, // Menu bar
-      {cols: 3, rows: 5, y: 1, x: 0}, // Layer list
-      {cols: 3, rows: 12, y: 6, x: 0}, // Context menu
-      {cols: 3, rows: 3, y: 18, x: 0}, // Menu
-      {cols: 5, rows: 10, y: 1, x: 3}, // Info
-      {cols: 5, rows: 10, y: 11, x: 3}, // View list
-      {cols: 12, rows: 2, y: 1, x: 8}, // Nav
-      {cols: 12, rows: 2, y: 19, x: 8}, // Status
-      {cols: 12, rows: 16, y: 3, x: 8} // Cesium app
-    ];
-    this.itemPosLayouts.layoutRightGlobe = {
-      menuBar: 0,
-      layerList: 1,
-      menuContext: 2,
-      menu: 3,
-      info: 4,
-      viewList: 5,
-      nav: 6,
-      status: 7,
-      globe: 8
+    this.itemPosLayouts = {
+      layoutLeftGlobe: {
+        menuBar: 0,
+        nav: 1,
+        status: 2,
+        info: 3,
+        viewList: 4,
+        layerList: 5,
+        menuContext: 6,
+        menu: 7,
+        globe: 8
+      },
+      layoutCenterGlobe: {
+        menuBar: 0,
+        layerList: 1,
+        menuContext: 2,
+        menu: 3,
+        nav: 4,
+        info: 5,
+        viewList: 6,
+        status: 7,
+        globe: 8
+      },
+      layoutRightGlobe: {
+        menuBar: 0,
+        layerList: 1,
+        menuContext: 2,
+        menu: 3,
+        info: 4,
+        viewList: 5,
+        nav: 6,
+        status: 7,
+        globe: 8
+      }
     };
   }
 
@@ -145,8 +147,8 @@ export class AppComponent implements OnInit {
     };
 
     // TODO Add/remove apps depending on the OS
-    this.dashboard = this.layouts.layoutCenterGlobe;
-    this.itemPos = this.itemPosLayouts.layoutCenterGlobe;
+    this.dashboard = this.layouts.layoutCenterGlobe.map(x => Object.assign({}, x)); // deep copy!
+    this.itemPos = Object.assign({}, this.itemPosLayouts.layoutCenterGlobe); // deep copy!
   }
 
   changeGridLayout(value: any) {
@@ -154,24 +156,37 @@ export class AppComponent implements OnInit {
     if (this.options.api && this.options.api.optionsChanged) {
       switch (value) {
         case "left": {
-          this.dashboard = this.layouts.layoutLeftGlobe;
-          this.itemPos = this.itemPosLayouts.layoutLeftGlobe;
+          this.dashboard = this.layouts.layoutLeftGlobe.map(x => Object.assign({}, x));
+          this.itemPos = Object.assign({}, this.itemPosLayouts.layoutLeftGlobe);
           break;
         }
         case "center": {
-          this.dashboard = this.layouts.layoutCenterGlobe;
-          this.itemPos = this.itemPosLayouts.layoutCenterGlobe;
+          this.dashboard = this.layouts.layoutCenterGlobe.map(x => Object.assign({}, x));
+          console.log(this.layouts.layoutCenterGlobe);
+          this.itemPos = Object.assign({}, this.itemPosLayouts.layoutCenterGlobe);
           break;
         }
         case "right": {
-          this.dashboard = this.layouts.layoutRightGlobe;
-          this.itemPos = this.itemPosLayouts.layoutRightGlobe;
+          this.dashboard = this.layouts.layoutRightGlobe.map(x => Object.assign({}, x));
+          this.itemPos = Object.assign({}, this.itemPosLayouts.layoutRightGlobe);
           break;
         }
       }
       this.options.api.optionsChanged();
     }
   }
+}
+
+interface GridLayouts {
+  layoutLeftGlobe: Array<GridsterItem>,
+  layoutCenterGlobe: Array<GridsterItem>,
+  layoutRightGlobe: Array<GridsterItem>
+}
+
+interface GridItemPositions {
+  layoutLeftGlobe: GridItemPos,
+  layoutCenterGlobe: GridItemPos,
+  layoutRightGlobe: GridItemPos
 }
 
 interface GridItemPos {
